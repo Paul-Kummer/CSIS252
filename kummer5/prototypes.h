fbb576e6057117e0e1e1878d2fe725cca24b97c3
Paul K <45641931+Paul-Kummer@users.noreply.github.com>
2020-10-26 20:38:28 -0500

    uploaded assignment 5
    Assignment 5 is to be used by other students to complete assignment 7

---
+++ kummer5/prototypes.h
@@ -0,0 +1,13 @@
+// File: prototypes.h
+// Name: Paul Kummer
+// Class: CSIS 252
+// Program: prog5
+// Modified: 10/20/20
+
+#pragma once
+#include "LightYear.h"
+#include <string>
+
+void printTest (std::string , LightYear );
+void printResult(LightYear , bool );
+void printOperations (std::string , std::string , LightYear , LightYear );
